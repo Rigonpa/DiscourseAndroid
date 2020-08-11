1405ff274d89431ab2962f83e987c477c378de59
Ricardo González Pacheco <rigonpa@gmail.com>
2020-08-11 12:54:24 +0200

    Coordinator layout

---
+++ app/src/main/java/com/example/eh_ho/topics/ScrollAwareFABBehaviour.kt
@@ -0,0 +1,71 @@
+package com.example.eh_ho.topics
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class ScrollAwareFABBehaviour(context: Context?, attrs: AttributeSet?) : FloatingActionButton.Behavior(context, attrs) {
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL || super.onStartNestedScroll(
+            coordinatorLayout,
+            child,
+            directTargetChild,
+            target,
+            axes,
+            type
+        )
+    }
+
+    override fun onNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: FloatingActionButton,
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int,
+        consumed: IntArray
+    ) {
+        super.onNestedScroll(
+            coordinatorLayout,
+            child,
+            target,
+            dxConsumed,
+            dyConsumed,
+            dxUnconsumed,
+            dyUnconsumed,
+            type,
+            consumed
+        )
+
+        if (dyConsumed > 0 && child.visibility == View.VISIBLE) {
+            child.hide()
+        } else if (dyConsumed < 0 && child.visibility != View.VISIBLE) {
+            child.show()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
